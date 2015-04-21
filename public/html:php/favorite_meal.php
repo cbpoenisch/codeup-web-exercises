11fe0e4309ee6c7f62f5df6419678548237af0d2
Crystal Poenisch <crystalpoenisch@Crystal-Poenischs-MacBook-Pro.local>
2015-04-20 21:27:38 -0500

    initial commit

---
+++ public/html:php/favorite_meal.php
@@ -0,0 +1,47 @@
+<?php var_dump($_GET) ?>
+
+<DOCTYPE! html>
+<html>
+
+	<head>
+		<title>Favorite Food</title>
+	</head>
+
+	<body>
+
+		<header>
+			<h1>Fruit Salad</h1>
+		</header>
+
+		<main>
+			<h2>Ingredients</h2>
+				<ol>
+					<li>Whipped Cream</li>
+					<li>Cocunut</li>
+					<li>Pecans</li>
+				</ol>
+
+		<form method="GET">
+			<p>
+				<label for="suggestion">Suggestions:</label>
+				<input type="text" id="suggestion" name="suggestion">
+			</p>
+			<p>
+				<input type="submit" value="Send">
+			</p>
+		</form>
+
+		</main>	
+
+		<aside>
+			<h3>Other foods you may like</h3>
+				<ul>
+					<li><a href="#" title="Link to food with whipped cream in the ingredients">Whipped Cream</a></li>
+					<li><a href="#" title="Link to food with coconut in the ingredients">Coconut Water</a></li>
+					<li><a href="#" title="Link to food with pecans in the ingredients">Pecan Pie</a></li>
+				</ul>
+		</aside>
+
+	</body>
+
+</html>
