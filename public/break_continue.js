25afb6e21c0b0dd8672b4c44fc8a292926f2e093
Crystal Poenisch <crystalpoenisch@Crystal-Poenischs-MacBook-Pro.local>
2015-04-27 19:12:05 -0500

    second commit

---
+++ public/break_continue.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <script>
+
+    	var random = Math.floor((Math.random()*50)+1);
+    	if (random % 2 == 0) 
+    	while(random % 2 == 0){ 
+    		Math.floor((Math.random()*50)+1);
+    	}
+    	console.log('Random number generated is ' . random);
+    
+    	for (var i = 1; i<= 50; i++) {
+
+    	}
+    </script>
+</body>
+</html>
